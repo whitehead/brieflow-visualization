540378aea7ed0c28eb62b39d89cc8f72e92cd9fb
Andy <andynu@gmail.com>
2025-04-18 09:24:38 -0400

    Add run-visualization.sh
    Starts the streamlit server.
    binds to 0.0.0.0 so you can get at it from another host (e.g. if you run
    it on the cluster you'll be able to get at it)

---
+++ run-visualization.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Activate venv if it exists
+if [ -d "venv" ]; then
+    source venv/bin/activate
+fi
+
+# Default port and config
+export STREAMLIT_CONFIG=".streamlit/config.toml"
+
+# Start Streamlit server, force bind to 0.0.0.0
+cd visualization
+exec streamlit run Home.py --server.address=0.0.0.0 "$@"
